d6d7b88fc2e7d9e10230ddf29383bfd5519ea5fb
shamikbose <shamik.bose89@gmail.com>
2020-06-10 16:21:36 -0400

    Package renamed
    SynthData renamed to synthdata to maintain PEP-8 naming conventions

--- readLeonData_All.py
+++ readLeonData_All.py
@@ -2,14 +2,14 @@ import pandas
 import numpy as np
 import pickle
 import os
-import SynthData
+import synthdata
 home_folder="../Covid-19/Leon/"
 #Get data for people
 people_file="people.txt"
 people_list={}
 people_data=np.genfromtxt(home_folder+people_file, skip_header=1, dtype=object)
 for count,row in enumerate(people_data):
-    people_list[count]=SynthData.Person(row)
+    people_list[count]=synthdata.Person(row)
 #Save data into serialized object
 pickle.dump(people_list,open("people_list_serialized.pkl","wb"))
 #Get data for households
@@ -17,7 +17,7 @@ households_file="households.txt"
 households_list={}
 households_data=np.genfromtxt(home_folder+households_file, skip_header=1, dtype=object)
 for count,row in enumerate(households_data):
-    households_list[count]=SynthData.Household(row)
+    households_list[count]=synthdata.Household(row)
 #Save household data into serialized object
 pickle.dump(households_list,open("households_list_serialized.pkl","wb"))
 #Get data for workplaces
@@ -25,7 +25,7 @@ workplaces_file="workplaces.txt"
 workplaces_list={}
 workplaces_data=np.genfromtxt(home_folder+workplaces_file, skip_header=1, dtype=object)
 for count,row in enumerate(workplaces_data):
-    workplaces_list[count]=SynthData.Workplace(row)
+    workplaces_list[count]=synthdata.Workplace(row)
 #Save household data into serialized object
 pickle.dump(workplaces_list,open("workplaces_list_serialized.pkl","wb"))
 #Get data for schools
@@ -33,6 +33,6 @@ schools_file="schools.txt"
 schools_list={}
 schools_data=np.genfromtxt(home_folder+schools_file, skip_header=1, dtype=object)
 for count,row in enumerate(schools_data):
-    schools_list[count]=SynthData.School(row)
+    schools_list[count]=synthdata.School(row)
 #Save household data into serialized object
 pickle.dump(schools_list,open("schools_list_serialized.pkl","wb"))
