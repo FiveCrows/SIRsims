0cbe0194600119bada9e5d39d6502610c5bffe71
Bryan Azbill <azbillbryan@gmail.com>
2020-06-15 13:24:34 -0400

    Update eventDrivenSIR.py

--- eventDrivenSIR.py
+++ eventDrivenSIR.py
@@ -5,11 +5,12 @@ import pandas as pd
 import random
 import pickle
 import itertools
-import PersonClass, HouseholdClass, WorkplaceClass
+import matplotlib.pyplot as plt
+import EoN
 
 epidemicSims = 10
-houseHolds = 600
-houseHoldSize = 4
+houseHolds = 10
+houseHoldSize = 500
 people = houseHoldSize * houseHolds
 schoolGroupSize = 20
 workGroupSize = 10
@@ -79,19 +80,6 @@ def p_attributeAssign(memberIndices, attributes, probabilities):
 #takes a dict of dicts to represent populace and returns a list of dicts of lists to represent groups of people with the same
 #attributes
 
-#connect list of groups with weight
-#TODO update to use a weight calculating function
-def clusterGroups(graph, groups, groupWeight):
-    for key in groups.keys():
-        print(key)
-        if key !=None:
-            memberCount = len(groups[key])
-            memberWeightScalar = np.sqrt(memberCount)
-            for i in range(memberCount):
-                for j in range(i):
-                    graph.add_edge(groups[key][i],groups[key][j] ,transmission_weight = groupWeight/memberWeightScalar)
-
-
 #for loading people objects from file
 def loadPickledPop(filename):
     with open(filename,'rb') as file:
@@ -124,14 +112,98 @@ def sortPopulace(populace, categories):
 
 
 
+#connect list of groups with weight
+#TODO update to use a weight calculating function
+def clusterDenseGroups(graph, groups, weight):
+    for key in groups.keys():
+        if key !=None:
+            memberCount = len(groups[key])
+            memberWeightScalar = np.sqrt(memberCount)
+            for i in range(memberCount):
+                for j in range(i):
+                    graph.add_edge(groups[key][i],groups[key][j] ,transmission_weight = weight/memberWeightScalar)
+
+
+def clusterByDegree_p(graph, groups, weight,degree_p):
+    #some random edges may be duplicates, best for large groups
+    connectorList = []
+
+    for key in groups.keys():
+        if key !=None:
+            memberCount = len(groups[key])
+            connectorList = []
+            for i in range(memberCount):
+                nodeDegree = random.choices(range(len(degree_p)), weights = degree_p)
+                connectorList.extend([i]*nodeDegree[0])
+            random.shuffle(connectorList)
+
+            i = 0
+            while i < len(connectorList)-1:
+                graph.add_edge(connectorList[i],connectorList[i+1],transmission_weight = weight)
+                i = i+2
+
+#clusters groups into strogatz small-worlds networks
+def strogatzDemCatz(graph, groups, weight, local_k, rewire_p):
+    if(local_k%2!=0):
+        print("Error: local_k must be even")
+
+    for key in groups:
+        if key!=None:
+            memberCount = len(groups[key])
+            if local_k >= memberCount:
+                print("warning: not enough members in group for .2f".format(local_k) + "local connections in strogatz net")
+                continue
+
+            group = groups[key]
+            #this unfinished won't rewire the same two nodes twice, but I decided to use a simpler alg anyways
+            #rewireCount = np.random.binomial(memberCount, rewire_p)
+            #rewireList = np.choices(group, rewireCount)*2
+            for i in range(memberCount):
+                if memberCount<5:
+                    print("stop")
+                for j in range(local_k//2):
+                    rewireRoll = random.uniform(0,1)
+
+                    if rewireRoll<rewire_p:
+                        graph.add_edge(i, (i + random.choice(range(memberCount - 1))) % memberCount, transmission_weight=weight)
+                    else:
+                        graph.add_edge(i, i + j, transmission_weight=weight)
+                    if rewire_p<rewireRoll<2*rewire_p:
+                        graph.add_edge(i, (i + random.choice(range(memberCount - 1))) % memberCount,transmission_weight=weight)
+                    else:
+                        graph.add_edge(i, i - j, transmission_weight=weight)
+
+#WIP
+def clusterGroupsByPA(graph, groups):
+    for key in groups.keys():
+        memberCount = len(groups[key])
+
+#def mergeSubClusterGraph(graph,subgraph, nodeMap):
 #def sortAttributes(people,attributeClasses):
 #populace = genPop(people, attributes, attribute_p)
 populace = loadPickledPop("people_list_serialized.pkl")
-populaceCategoryGroups = sortPopulace(populace,['sp_hh_id','work_id'])
-
+popsByCategory = sortPopulace(populace, ['sp_hh_id', 'work_id', 'school_id'])
 graph = nx.Graph()
-for category in populaceCategoryGroups:
-    clusterGroups(graph, populaceCategoryGroups[category], 0.1)
+
+clusterDenseGroups(graph, popsByCategory['sp_hh_id'],1)
+clusterByDegree_p(graph,popsByCategory['work_id'], 1, [0,0,0.2,0.3,0.5])
+clusterByDegree_p(graph,popsByCategory['school_id'], 1, [0,0,0.2,0.3,0.5])
+
+#strogatzDemCatz(graph, popsByCategory['work_id'], 1, 4, 0.2)
+
+
+node_investigation = EoN.fast_SIR(graph, globalInfectionRate, recoveryRate, rho = 0.01, transmission_weight ='transmission_weight',return_full_data = True)
+if not nx.is_connected(graph):
+    print("warning: graph is not conneted, the are .2f{} components".format(nx.number_connected_components(graph)))
+
+plt.plot(node_investigation.summary(popsByCategory['work_id'][None])[1]['I'],label = "infected students")
+plt.legend()
+plt.show()
+
+
+plt.plot("this")
+
+
 
 print("stop here ")
 
@@ -147,17 +219,8 @@ print("stop here ")
 #Idea: track neighborhoods and whole city as 'global groups', groups which occur global infections to eachother, but aren't necessarily bigger risks because the group is bigger
 #these are contacts that occur between strangers who possibly share the same transit, gym, etc.
 
-#TODO a function that returns the infection rate of neighborhoods and or city
-
-
-
 
 
 #TODO a function to animate a graph in time
 
 #TODO write a function to spline 1d t,S,I,R arrays into even time intervals so that multiple runs can be averaged
-
-#TODO a function to draw a sparse weights matrix in a graph
-
-
-
