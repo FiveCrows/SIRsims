6284cff6fb50e7ad101c142bac7b6ebb21683757
shamikbose <shamik.bose89@gmail.com>
2020-07-20 12:58:55 -0400

    Revert "Merge branch 'master' of https://github.com/Shponglic/SIRsims"
    This reverts commit b152bb2548e6cc5269c94b78957796afb0b4e13c, reversing
    changes made to cc86dbff6cae32557bd6e9c0884bb2e7e24a56d5.

--- ContagionTest.py
+++ ContagionTest.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 mask_scalar = 0.3
 env_weights = {"school_id": 0.1 , "work_id": 0.2, "sp_hh_id": 1}
-env_degrees = {'work': 16, 'school' : 20}
+env_degrees = {'work': 16, 'school' :20}
 env_masking = {'work': 0, 'school':0}
 gamma = 0.1
 tau = 0.08
@@ -11,10 +11,7 @@ weighter = TransmissionWeighter(env_weights, mask_scalar)
 model = PopulaceGraph(weighter, env_degrees, env_masking)
 contactMatrixUSA_Base = pd.read_csv("./ContactMatrices/Base/ContactMatrixUSA_Base.csv").values
 model.build(model.clusterStrogatz)
-model.plotContactMatrix('age', 5)
-model.simulate(gamma, tau, title = "Control test")
-model.fitWithContactMatrix(contactMatrixUSA_Base, 'age', 5)
-model.plotContactMatrix('age', 5)
-model.build(model.clusterStrogatz)
-model.simulate(gamma, tau, title = "After Simulation")
 model.plotSIR()
+model.fitGraphToContactMatrix(contactMatrixUSA_Base, 'age', 5)
+model.build(model.clusterStrogatz)
+model.simulate(gamma, tau)
