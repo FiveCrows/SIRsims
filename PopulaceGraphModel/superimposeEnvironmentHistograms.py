238c99d4ca58c3c05ef3e2656eb5969b38dcaaed
erlebach <gordon.erlebach@gmail.com>
2020-09-21 23:27:04 -0400

    School population in Brian's code is different than mine. Why?

--- PopulaceGraphModel/superimposeEnvironmentHistograms.py
+++ PopulaceGraphModel/superimposeEnvironmentHistograms.py
@@ -19,8 +19,8 @@ names = ["{}:{}".format(5 * i, 5 * (i + 1)) for i in range(15)]
 partition = Partitioner(enumerator, 'age', names)
 
 # Choose one or the other model
-which_model = 'random_GE'           # AB contact graph algorithm
-#which_model = 'strogatz_AB'         # GE contact graph algorithm (makeGraph)
+which_model = 'random_GE'           # GE contact graph algorithm
+#which_model = 'strogatz_AB'         # AB contact graph algorithm (makeGraph)
 
 model = PopulaceGraph( partition, slim = True)
 
@@ -35,34 +35,46 @@ elif which_model == 'random_GE':
 #workplaces = list(filter(lambda environment: model.environments[environment].type == 'workplace' and model.environments[environment].population>25, model.environments))
 schools = sorted(list(filter(lambda environment: model.environments[environment].type == 'school', model.environments)), key = lambda environment: model.environments[environment].population)
 workplaces = sorted(list(filter(lambda environment: model.environments[environment].type == 'workplace', model.environments)), key = lambda environment: model.environments[environment].population)
-num_plots = 25
+
+lst = schools
+for index in reversed(lst):
+    print("pop= ", model.environments[index].population)
+quit()
 
 
-
-
-for list in [schools, workplaces]:
-    fig, ax = plt.subplots(5, 5)
+# list is a command. Do not use as a variable
+for lst in [schools, workplaces]:
+    nrows, ncols = 5, 5
+    fig, ax = plt.subplots(nrows, ncols)
+    num_plots = nrows * ncols
     fig.set_size_inches(18.5, 10.5)
     plot_num = 0
-    for index in reversed(list[-num_plots:]):
+
+    for index in reversed(lst[-num_plots:]):
         environment = model.environments[index]
         people = environment.members
+        pop = environment.population
+        print("pop= ", pop)
 
         graph = model.graph.subgraph(people)
-
         degreeCounts = [0] * 40
+
         for person in people:
             try:
                 degree = len(graph[person])
             except:
                 degree = 0
-            degreeCounts[degree] += 1 / environment.population
-        ax[plot_num//5, plot_num%5].plot(range(len(degreeCounts)), degreeCounts, label="Population: {}".format(environment.population))
-        plot_num +=1
+            degreeCounts[degree] += 1 / pop
+
+        axx = ax[plot_num//5, plot_num%5]
+        axx.plot(range(len(degreeCounts)), degreeCounts,\
+             label="Population: {}".format(pop))
+        axx.set_title("(GE) N="+str(pop))
+        plot_num += 1
 
     plt.ylabel("total people")
     plt.xlabel("degree")
-    plt.title("histogram for top 25 {}s using ".format(model.environments[list[0]].type) + name)
+    #plt.title("histogram for top 25 {}s using ".format(model.environments[lst[0]].type) + name)
     plt.legend()
     plt.show()
 
