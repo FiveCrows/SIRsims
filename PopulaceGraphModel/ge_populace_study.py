4bf4ff1c6e81feafc7c6809506f57d218c76ccce
erlebach <gordon.erlebach@gmail.com>
2020-10-03 00:24:30 -0400

    Rewriting ge_ModelToolkit.py

--- PopulaceGraphModel/ge_populace_study.py
+++ PopulaceGraphModel/ge_populace_study.py
@@ -1,24 +1,35 @@
-from ModelToolkit import *
+from ge_ModelToolkit import *
 import copy
 # plot chance of infection
 
-default_env_scalars = {"school": 0.3, "workplace": 0.3, "household": 1}
-env_degrees = {'workplace': None, 'school': None}
-default_env_masking = {'workplace': 0, 'school':0, 'household': 0}
-workplace_preventions = {'masking': 0, 'distancing': 0}
-school_preventions = {'masking':0, 'distancing': 0}
-household_preventions = {'masking':0, 'distancing':0}
-preventions = {'workplace': workplace_preventions, 'school': school_preventions, 'household': household_preventions}
-prevention_reductions = {'masking': 0.1722, 'distancing': 0.2071}# dustins values
-trans_weighter = TransmissionWeighter(default_env_scalars, prevention_reductions)
-gamma = 0.1
-tau = 0.08
-enumerator = {i:i//5 for i in range(75)}
-enumerator.update({i:15 for i in range(75,100)})
-names = ["{}:{}".format(5 * i, 5 * (i + 1)) for i in range(15)]
-partition = Partitioner('age', enumerator, names)
-model = PopulaceGraph( partition, slim = False)
+#################################################################################
+#####   Begin setting up model variables  #######################################
+#################################################################################
 
+default_env_scalars   = {"school": 0.3, "workplace": 0.3, "household": 1}
+env_degrees           = {'workplace': None, 'school': None}
+default_env_masking   = {'workplace': 0, 'school':0, 'household': 0}
+workplace_preventions = {'masking': 0, 'distancing': 0}
+school_preventions    = {'masking':0, 'distancing': 0}
+household_preventions = {'masking':0, 'distancing':0}
+preventions           = {'workplace': workplace_preventions, 'school': school_preventions, 'household': household_preventions}
+prevention_reductions = {'masking': 0.1722, 'distancing': 0.2071}# dustins values
+trans_weighter        = TransmissionWeighter(default_env_scalars, prevention_reductions)
+gamma                 = 0.1
+tau                   = 0.08
+names                 = ["{}:{}".format(5 * i, 5 * (i + 1)) for i in range(15)]
+
+# Dictionary of ages: enumerator[i] => age bracket[i]
+# Age brackets: range(0,5), range(5:10), ..., range(70:75), range(75:100)
+enumerator            = {i:i//5 for i in range(75)}
+enumerator.update({i:15 for i in range(75,100)})
+
+#################################################################################
+#####   End setting up model variables  #######################################
+#################################################################################
+
+partition = Partitioner('age', enumerator, names)
+model     = PopulaceGraph( partition, slim = True)
 model.build(trans_weighter, preventions, env_degrees)
 model.simulate(gamma, tau, title = 'base-test')
 
