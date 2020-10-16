2278ebde629cc6cb56a49d56b2022d23eaa74218
Gordon Erlebacher <erlebach@Gordons-MBP-2.Home>
2020-10-15 23:31:32 -0400

    Fixed transform_analysis.py . Correctly compute time of maxI

--- PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/transform_analysis.py
+++ PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/transform_analysis.py
@@ -19,18 +19,29 @@ rows = {}
 N_col = []
 curves_col = []
 maxI_col = []
+t_maxI_col = []
 #print(df.columns)
 
 for row_tuple in df.itertuples(): 
     N = []
     maxI = []
+    #argmaxI = []
+    t_maxI = []
     curves = u.generateCurves(row_tuple)
+    #print("curves.keys(): ", curves.keys())
     for k in range(nb_age_groups):
         maxI.append( np.max(curves[k]['I']) )
+        # time at which infection is maximum
+        print("curves[k]['t']= ", curves[k]['t'])
+        print("curves[k]['I']= ", curves[k]['I'])
+        t_maxI.append( curves[k]['t'][np.argmax(curves[k]['I'])] )
+        #print("argmax: ", np.argmax(curves[k]['I']))
+        #print("aa= ", aa)
         N.append( curves[k]['S'][0] + curves[k]['I'][0] + curves[k]['R'][0] )
     N_col.append(N)
     curves_col.append(curves)
     maxI_col.append(maxI)
+    t_maxI_col.append(t_maxI)
     # add columns for N and curves: that is another 38 columns
     # remove column
 
@@ -41,16 +52,18 @@ dfc.drop("ages", axis=1)
 
 dfc['N_age'] = N_col
 dfc['maxI_age'] = maxI_col
+# Accuracy to within two days 
+dfc['argmaxI_age'] = t_maxI_col
 dfc['SIR_age'] = curves_col
 
 
 #print(dfc.head(10))
 #print("columns: ", dfc.columns)
-print(dfc.maxI_age)
-print(dfc.N_age)
+#print(dfc.maxI_age)
+#print(dfc.N_age)
 dfc.to_pickle("transformed_metadata.gz")
 dfc.to_csv("transformed_metadata.csv")
 
-print("dfc.columns: ", dfc.columns)
+#print("dfc.columns: ", dfc.columns)
 quit()
 
--- PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/utils.py
+++ PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/utils.py
@@ -3,29 +3,45 @@ import numpy as np
 #---------------------
 def generateCurves(row):
     ages = row.ages
+    # ages.keys(): 2, 4, 6, ... : age time levels
+    #print("ages.keys(): ", ages.keys())
+    # ages[2].keys(): age brackets: 0, 1, ..., 18, 19
+    #print("ages[2].keys(): ", ages[2].keys())
+    # ages[2][4].keys(): 'S', 'I', 'R'
+    #print("ages[2][4].keys(): ", ages[2][4].keys())
+    # ages[2][4]['S']: 3818 (one value of susceptibility)
+    #print("ages[2][4]['S']: ", ages[2][4]['S'])
+
     curves = {}
+    times = np.sort(list(ages.keys()))
+
 # I want curves[0-19].S = [S[0], S[10], ...]
 # I want curves[0-19].I = [I[0], I[10], ...]
 
-    for k in range(0,20):
-        curves[k] = {}
-        curves[k]['S'] = []
-        curves[k]['I'] = []
-        curves[k]['R'] = []
-        curves[k]['t'] = []
+    for k_age in ages[2].keys():  # list of age brackets
+        curves[k_age] = {}
+        curves[k_age]['S'] = []
+        curves[k_age]['I'] = []
+        curves[k_age]['R'] = []
+        curves[k_age]['t'] = []
 
-    for k, v in ages.items():
-        # For each time, extract SIR curves for all ages
-        # k is the time level, v is the SIR data at that time
-        ages_l = v
-        for k_age, v_age in v.items():   # 1 .. 19
-            curves[k_age]['S'].append(v_age['S'])
-            curves[k_age]['I'].append(v_age['I'])
-            curves[k_age]['R'].append(v_age['R'])
-            curves[k_age]['t'].append(k)
+    #print("ages.items()= ", ages.items())
+
+    for age_t in ages.keys():   # age SIR time levels
+        ages_sir = ages[age_t]
+        #print("ages_sir: ", ages_sir)
+        for k,v in ages_sir.items():
+            # For each time, extract SIR curves for all ages
+            # k is the time level, v is the SIR data at that time
+            #for k_age, v_age in v.items():   # 1 .. 19
+            #for k_age, v_age in v.items():   # 1 .. 19
+            curves[k]['S'].append(v['S'])
+            curves[k]['I'].append(v['I'])
+            curves[k]['R'].append(v['R'])
+            curves[k]['t'].append(age_t)
 
     # Convert to numpy array
-    for k_age in range(0,20):
+    for k_age in ages[2].keys():
         curves[k_age]['S'] = np.asarray(curves[k_age]['S'])
         curves[k_age]['I'] = np.asarray(curves[k_age]['I'])
         curves[k_age]['R'] = np.asarray(curves[k_age]['R'])
