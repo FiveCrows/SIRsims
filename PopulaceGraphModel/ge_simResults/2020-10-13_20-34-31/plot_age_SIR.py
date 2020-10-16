106bee0ddb374aee6cc2fb6d8c2aca2d8c027b3e
Gordon Erlebacher <erlebach@Gordons-MBP-2.Home>
2020-10-16 16:40:14 -0400

    Updated versions. They work.

--- PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/plot_age_SIR.py
+++ PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/plot_age_SIR.py
@@ -176,9 +176,110 @@ def plotI_reduction(df0, dct, title):
     quit()
 
 #-----------------------------------------
-dfs0 = dfs[(.7,.7,.7,.7)]
+def plotI_reduction_subcellsOnly(df0, dct, subcells, title):
+    # Only plot Infection curves listed in subcells dictionary
+    print(df0.columns)
+    rows = df0[(df0["red_mask"] == 0.6) & (df0["red_dist"] == 0.6)]
+    row = rows.iloc[0]
+
+    title = "Infectivity by reduction (normalized age group pop size)\n" 
+    filnm = "ageSIR_"
+
+    count = 0
+    for k,v in dct.items():
+        if count == 0:
+            comma = ""
+        else:
+            comma = ","
+        count += 1
+        title += comma + " %s=%2.1f" % (k,v) 
+        filnm += comma + "%s=%2.1f" % (k,v) 
+
+    fig, axes = plt.subplots(4,5, figsize=(10,8))
+    fig.suptitle(title)
+    axes = axes.flatten()
+
+    reductions = [r for r in np.linspace(0, 10., 6)/10]
+    N_age =  df0.iloc[0].N_age
+
+    # Create empty plot with blank marker containing the extra label
+
+    xticks = np.linspace(0,50,6).astype('int')
+    xnames = np.vectorize(str)(xticks)
+    #yticks = np.linspace(0,5,6) / 10.
+    #ynames = np.vectorize(str)(yticks)
+
+    for k in range(0,19):  # age brackets 0-18
+        ax = axes[k]
+        #ax.plot([], [], ' ', label="Reduction (%)")
+        N_age_list = []
+        I_to_plot = subcells[k]
+        for i,j in I_to_plot:
+            rm, rd = reductions[i], reductions[j]
+            # if rm, rd not in the list to plot, skip
+        #for red in reductions:
+            rows = df0[(df0["red_mask"] == rm) & (df0["red_dist"] == rd)]
+            row = rows.iloc[0]
+            SIR = row.SIR_age
+            N_age = row.N_age[k]
+            N_age_list.append(N_age)
+            S,I,R,t = [SIR[k][l] for l in ['S','I','R','t']]
+            ax.plot(t, I/N_age, label="%3d"%(100*rm))
+            ax.set_xlim(0,50)
+            ax.set_ylim(0,.005)
+            # Must call xticks before labels for proper positioning
+            ax.set_xticks(xticks)
+            ax.set_xticklabels(xnames, fontsize=6)
+            #ax.set_yticks(yticks)
+            #ax.set_yticklabels(ynames, fontsize=6)
+            ax.tick_params(axis='both', which='both', length=0)
+
+        print("=== k= ", k)
+        age_str = str(5*k)+"-"+str(5*k+4)
+        print("N_age_list= ", N_age_list)
+        ax.set_title(age_str+", N=%d" % N_age_list[0], fontsize=10)
+        l = ax.legend(fontsize=6, framealpha=0.5, title="Reduction (%)")
+        plt.setp(l.get_title(), fontsize=8)
+        ax.grid(True)
+
+    plt.tight_layout()
+    plt.savefig(filnm+".pdf")
+    quit()
+
+#-----------------------------------------
 dfs0 = dfs[(.3,.3,.3,.3)]
+dfs0 = dfs[(.7,.7,.7,.7)]
+#-----------------------------------------
+dfs0 = dfs[(.3,.3,.3,.3)]
+dfs0 = dfs[(.7,.7,.7,.7)]
 #plotAgeSIR(dfs0, dct, title="")
 dct = getParams(dfs0, group_key)
-plotI_reduction(dfs0, dct, title="")
+#plotI_reduction(dfs0, dct, title="")
  
+
+# Display the curves for these cases
+ages = {}
+ages[0] = [(5,4),(5,5)]
+ages[1] = [(5,4),(5,5)]
+ages[2] = [(5,5)]
+ages[3] = [(5,4),(5,5)]
+ages[4] = [(5,4),(5,5),(4,5)]
+ages[5] = [(5,4),(5,5)]
+ages[6] = [(5,4),(5,5)]
+ages[7] = [(5,4),(5,5)]
+ages[8] = [(5,4),(5,5)]
+ages[9] = [(5,5)]
+ages[10] = [(5,4),(5,5)]
+ages[11] = [(5,4),(5,5),(4,5)]
+ages[12] = [(5,4),(5,5),(4,5),(5,3)]
+ages[13] = [(5,4),(5,5),(4,5)]
+ages[14] = [(5,4),(5,5),(5,3)]
+ages[15] = [(4,5),(5,3),(5,4),(5,5)]
+ages[16] = [(5,1),(5,2),(5,3),(5,4),(5,5)]
+ages[17] = [(5,4),(5,5),(4,5)]
+ages[18] = [(5,0),(5,1),(5,3),(5,4),(5,5),(4,3),(4,4),(4,5),(3,4),(3,5),(2,5)]
+ages_to_plot = ages
+
+dfs0 = dfs[(.7,.7,.7,.7)]
+dct = getParams(dfs0, group_key)
+plotI_reduction_subcellsOnly(dfs0, dct, ages_to_plot, title="")
--- PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/plot_t_max_infectivity.py
+++ PopulaceGraphModel/ge_simResults/2020-10-13_20-34-31/plot_t_max_infectivity.py
@@ -6,6 +6,7 @@ import utils as u
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Rectangle
 import matplotlib.patches as mpatches
+import matplotlib.colors as _colors
 
 # Return dictionary parameter/values of first row of a DataFrame
 def getParams(dfs, params):
@@ -35,9 +36,6 @@ for key, gr in dfg:
     keys.append(key)
     dfs[key]= dfg.get_group(key)
 
-dfs0 = dfs[(.3,.3,.3,.3)]
-dfs0 = dfs[(.7,.7,.7,.7)]
-dct = getParams(dfs0, group_key)
 #-----------------------------------------------------
 
 #---------------------
@@ -48,76 +46,67 @@ dct = getParams(dfs0, group_key)
 #---------------------
 
 #---------------------
-# Plot a matrix of the maximum infected percentage as a function of reductions
-def computeArgmaxInfections(age_bracket, df0):
+# return a matrix of the time of maximum infections and a matrix
+# of maximum infections
+def computeTMaxInfections(age_bracket, df0):
     # I AM CALCULATING REDUNDANT STUFF.
     k = age_bracket
     mat = np.zeros([6,6])   # <<<<
+    mat_maxI = np.zeros([6,6], dtype='int')   # <<<<
     mat_d = {}
+    mat_maxI_d = {}
     for r in df0.itertuples():  # loop over rows
         #print("r.argmaxI_age: ", r.argmaxI_age)
         curve = r.SIR_age[k]
         red_mask = r.red_mask
         red_dist = r.red_dist
         t_maxI = r.t_maxI_age[k]    # time of max infectivity
-        print("t_maxI= ", t_maxI, len(curve['S']), len(curve['t']))
-        print(curve['t'])
+        maxI = r.maxI_age[k]    # time of max infectivity
+        #print("t_maxI= ", t_maxI, len(curve['S']), len(curve['t']))
+        #print(curve['t'])
         N_age = r.N_age[k]
         mat_d[(red_mask,red_dist)] = t_maxI
+        mat_maxI_d[(red_mask,red_dist)] = maxI
 
     # How to convert this information into a matrix
+    print("mat_d= ", mat_d)
+    print("mat_maxI_d= ", mat_maxI_d)
     red = np.linspace(0.,10,6) / 10.  # to avoid 0.3000004 <<<<< ERROR?
     for i,r1 in enumerate(red):
         for j,r2 in enumerate(red):
             try:
                 mat[i,j] = mat_d[r1,r2]
+                mat_maxI[i,j] = mat_maxI_d[r1,r2]
             except:
                 print("pass")
                 pass
-    return mat, N_age
+    return mat, mat_maxI, N_age
 
 #------------------------
-def myLinePlot(ax, mat, cmap):
-    nx = ny = 6
-    xx = np.asarray(range(6)) / 5.
-    yy = np.asarray(range(6)) / 5.
-
-    def mycolor(val):
-        return plt.get_cmap(cmap)(val)
-
-    colors = []
-    rects  = []
-
-    for x in range(nx):
-        for y in range(ny):
-            rect = mpatches.Rectangle((xx[x]-.1,yy[y]-.1), .2, .2) #, color=mycolor(mat[x,y]))
-            #print(mat[x,y])
-            rects.append(rect)
-            colors.append(mycolor(1.5*mat[x,y]))
-
-    pc = PatchCollection(rects) #, cmap='hot', array=None)
-    pc.set(array=None, facecolors=colors)
-    ax.add_collection(pc)
-
-#-------------------------
-def myImgshow(ax, mat, cmap):
+def myImgshow(ax, mat, mat_maxI, cmap):
     nx = ny = 6
     xx = np.asarray(range(6)) / 5.
     yy = np.asarray(range(6)) / 5.
     plt.rcParams['image.cmap'] = 'gray'
 
+    print("colors= ", _colors)
+    print(dir(_colors))
+    norm = _colors.LogNorm(vmin=0.005, vmax=1.0)
+
     def mycolor(val):
-        return plt.get_cmap(cmap)(val)
+        #return plt.get_cmap(cmap)(val)
+        return plt.get_cmap(cmap)(norm(val))
 
     colors = []
     rects  = []
+    print("myImgshow:mat_maxI: ", mat_maxI.shape)
+    print(mat_maxI)
 
     for x in range(nx):
         for y in range(ny):
             rect = mpatches.Rectangle((xx[x]-.1,yy[y]-.1), .2, .2) #, color=mycolor(mat[x,y]))
-            print(mat[x,y])
             rects.append(rect)
-            colors.append(mycolor(mat[x,y]/140.))  # 140: max value of t_argmax
+            colors.append(mycolor(mat[x,y]/30.))  # 140: max value of t_argmax
 
     pc = PatchCollection(rects) #, cmap='hot', array=None)
     pc.set(array=None, facecolors=colors)
@@ -125,8 +114,8 @@ def myImgshow(ax, mat, cmap):
 
 
 #------------------------
-def plotArgmaxInfections(df0, dct, title):
-    filnm = "argmax_infectivity"
+def plotTMaxInfections(df0, dct, title):
+    filnm = "t_maxI_infectivity"
     for k,v in dct.items():
         filnm += "_%s=%3.2f" % (k,v)
 
@@ -153,20 +142,21 @@ def plotArgmaxInfections(df0, dct, title):
 
     for k in range(19):
         if 1:
-            mat, Nage = computeArgmaxInfections(k, df0)
+            mat, mat_maxI, Nage = computeTMaxInfections(k, df0)
             ax = axes[k]
             ax.xaxis.set_ticks_position('bottom')
-            #myLinePlot(ax, mat, cmap='hot')
-            myImgshow(ax, mat, cmap='hot')
+            myImgshow(ax, mat, mat_maxI, cmap='hot_r')
+            #myImgshow(ax, mat, cmap='bwr')
             ax.set_xlim(-0.1, 1.1)
             ax.set_ylim(-0.1, 1.1)
             nx,ny = len(names), len(names)  # not general
 
             for i in range(nx):
               for j in range(ny):
+                  #txt = "%3d" % mat_maxI[i,j]
                   txt = "%3d" % mat[i,j]
-                  if mat[i,j] > 30: col = 'black'
-                  else: col = 'w'
+                  if mat[i,j] > 15: col = 'white'
+                  else: col = 'black'
                   text = ax.text(i*0.2, j*0.2, txt, ha="center", va="center", color=col, fontsize=4)
 
             # Nage: nb of people in that age category
@@ -179,9 +169,15 @@ def plotArgmaxInfections(df0, dct, title):
             ax.set_yticks(ticks)
             ax.tick_params(axis='both', which='both', length=0)
     plt.tight_layout()
-    #plt.show()
     plt.savefig(filnm + ".pdf")
     
-plotArgmaxInfections(dfs0, dct, title="Time of maximum infectivity by age category")
+dfs0 = dfs[(.3,.3,.3,.3)]
+dct = getParams(dfs0, group_key)
+plotTMaxInfections(dfs0, dct, title="Time of maximum infectivity by age category")
+
+dfs0 = dfs[(.7,.7,.7,.7)]
+dct = getParams(dfs0, group_key)
+plotTMaxInfections(dfs0, dct, title="Time of maximum infectivity by age category")
+
 quit()
 #-----------------------------------------------------
