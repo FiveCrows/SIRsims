be1e69ab905bfa42c153a32fc4e3d87084e1ad27
Gordon Erlebacher <erlebach@Gordons-MBP-2.Home>
2020-10-31 09:31:40 -0400

    Remainder files need to regenerate the images.

---
+++ PopulaceGraphModel/ge_simResults_good/2020-10-31_08-30-51/src/analyze.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import glob
+import scanf
+import pandas as pd
+
+
+def readFile(filenm):
+    fd = open(filenm, "rb")
+    d = pickle.load(fd)
+    ages_SIR = d['ages_SIR']
+    #print("ages_SIR= ", ages_SIR)
+
+    dct = d
+    for k in dct.keys():
+        print("dictionary key: ", k)
+    print()
+    global_dict = d['global_dict']
+    for k in global_dict.keys():
+        print("glob_dict key: ", k)
+
+    print("cv_key= ", global_dict["cv_key"])
+    print("cv_val= ", global_dict["cv_val"])
+    if global_dict["cv_key"] == "contact":
+        print("CONTACT FOUND")
+    else:
+        print("CONTACT NOT FOUND")
+    # THERE IS A BUG!!!
+    #quit()
+
+    dct['cv_key']  = global_dict['cv_key']
+    dct['cv_val']  = global_dict['cv_val']
+
+    """
+    dct = {'sm':sm, 'sd':sd, 'wm':wm, 'wd':wd, 'red_mask': red_mask, 'red_dist': red_dist, 'v': v}
+    dct['tau'] = d['params_dict']['tau']
+    dct['gamma'] = d['params_dict']['gamma']
+    dct['SIR'] = SIR
+    dct['fn'] = filenm
+    dct['preventions'] = d['preventions']
+    dct['prevention_reductions'] = d['prevention_reductions']
+    dct['vaccination_dict'] = d['vaccination_dict']
+    print("vaccination_dict: ", dct["vaccination_dict"])
+    """
+
+    print("title: ", dct["title"])
+
+    #dct['SIR'] = d['sim_results']  # temporary
+    dct['ages'] = {}
+    print("preventions: ", dct["preventions"])
+    print("prevention_prevalences: ", dct["prevention_prevalences"])
+
+    for k,v in ages_SIR.items():
+        ages = dct['ages']
+        ages[k] = v
+
+    return dct
+
+#--------------------------------------------
+dicts = []
+files = glob.glob("conta*") + glob.glob("mask*") + glob.glob("weigh*")
+
+for filenm in files:
+    print("--------------------------------")
+    print("filenm: ", filenm)
+    dicts.append( readFile(filenm) )
+
+df = pd.DataFrame.from_dict(dicts)
+df.to_pickle("metadata.gz")
+df.to_csv("metadata.csv")
+
+print("-------------------")
+#readFile("red_mask=0.50,red_dist=0.50,sm=0.50,sd=0.50,wm=0.50,wd=0.50,v=0.99, gamma=0.2, tau=0.2, 2020-10-19,12-37-07")
