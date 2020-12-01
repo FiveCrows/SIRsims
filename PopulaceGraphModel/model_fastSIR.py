fd8af0d90c6ef4458529d49015028da6ef176ae4
Gordon Erlebacher <erlebach@Gordons-MBP-2.Home>
2020-12-01 18:29:01 -0500

    Added computation of histogram of generation times.
    Forward and backward generation times should be the same in this code.
    But this should be checked. The graph is fully connected with  1000
    nodes.

--- PopulaceGraphModel/model_fastSIR.py
+++ PopulaceGraphModel/model_fastSIR.py
@@ -2,6 +2,71 @@ import networkx as nx
 import EoN as eon
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
+
+
+def processTransmissionTimes(trans):
+    # trans is a list of tuples (t,u,v): u infected v at time t
+    # Times are ordered chronologically
+
+    # I would like to identify the time u got infected. 
+    # Sort the list by t
+
+    # Given tu = (t,u,v), find the infection time of u
+    # For that, must find (t1, u1, u). Note that t1 < t
+    # Need dictionary: who[u] to return u1
+
+    t0,u0,v0 = trans[0]
+    print("trans[0]= ", t0,u0,v0)
+    # u can infect multiple v
+    # I should be able to construct the forward generation (fg) histogram across 
+    # the population and binned per day. 
+    # Also create dictionary of when each node is infected
+
+    fwd = defaultdict(list)
+    infect = defaultdict(float)
+
+    for t,u,v in trans:
+        fwd[u].append((t,v))
+        infect[v] = t
+
+    # construct time intervals
+    gen_times = []
+    for t,u,v in trans:
+        gen_times.append(infect[v] - infect[u])
+
+    # superimpose a range of exponentials (theoretically the case)
+    def expf(t, lmbda):
+        return lmbda * np.exp(-lmbda*t)
+
+    t = np.linspace(0., 0.6, 20)
+    plt.hist(gen_times, density=True)
+    #for v in [10,12,14,16,18,20]:
+    for v in [12]:
+        plt.plot(t, expf(t, v), label=v)
+    plt.legend(fontsize=10)
+    plt.show()
+    quit()
+
+    #print("fwd= ", fwd)
+    #for k,v in fwd.items():
+        #print(k,v)
+
+    # Create a histogram of how many people a person infected
+    nb_people = defaultdict(int)
+    for k,v in fwd.items():
+        nb = len(v)
+        nb_people[nb] += 1
+
+    # Sort by keys
+    lst = sorted(zip(nb_people.keys(), nb_people.values()))
+    x, y = zip(*lst)
+    print(lst)
+    #plt.bar(x, y)
+    #plt.show()
+
+
+#---------------------------------
 
 # Example SIR model using fast_SIR, based on an N to N graph (homogeneous connections. The graph is undirected. Transmission and recovery parameters are constant. 
 
@@ -10,7 +75,7 @@ gamma = .02  # Recovery (I -> R)  [1/days]  (per node)
 
 # A graph with N nodes has N*(N-1)/2 edges. 
 
-N = 100  # number of nodes
+N = 1000  # number of nodes
 G = nx.complete_graph(N)
 nb_nodes = G.number_of_nodes()
 
@@ -48,6 +113,15 @@ sim_result = eon.fast_SIR(G, beta, gamma, initial_infecteds=initial_infected,
 times = sim_result.t()
 statuses = {}
 last_time = times[-1]
+
+# trans: list of (t,u,v): u infected v at time t
+trans = sim_result.transmissions()
+trans_tree = sim_result.transmission_tree()
+print("nodes: ", trans_tree.number_of_nodes())
+print("edges: ", trans_tree.number_of_edges())
+print(trans)
+processTransmissionTimes(trans)
+quit()
 
 # Specify the times you wish to work with. Tix is the time index. 
 for tix in range(0, int(last_time)+2, 1):
