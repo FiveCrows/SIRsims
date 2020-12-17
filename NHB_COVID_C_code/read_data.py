926c0bba6aa4e7182bdaf7e5ba658937ebeb06d3
Gordon Erlebacher <erlebach@Gordons-MBP-2.Home>
2020-12-17 14:23:16 -0500

    Make code more readable.

--- NHB_COVID_C_code/read_data.py
+++ NHB_COVID_C_code/read_data.py
@@ -6,6 +6,7 @@ import numpy as np
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 filenm = "Results/data_baseline_p0.txt"
 
@@ -38,25 +39,23 @@ by = df.groupby("run")
 def plot_group(by, group):
     # Different groups have different lengths
     df = by.get_group(group)
-    infected_s  = df["i_sympt"];
-    pre_sympt = df["pre_sympt"]
-    lat_sympt   = df["l_sympt"]
+    infected_s      = df["i_sympt"];
+    pre_sympt       = df["pre_sympt"]
+    lat_sympt       = df["l_sympt"]
     infected_total  = infected_s + lat_sympt + pre_sympt  
-    recov     = df["recov"]
+    recov           = df["recov"]
 
-    alpha=0.8
+    names = [infected_s, pre_sympt, lat_sympt, recov, infected_total, infected_total]
+    labels = ["infectious_s", "pre_s", "latent_s", "recovered", "total_infected"]
+    colors = ["r", "orange", "b", "g", "black"]
     handles = []
-    p, = plt.plot(range(len(infected_s)),   infected_s,  alpha=alpha, color='r', label="infectious_s")
-    handles.append(p)
-    p, = plt.plot(range(len(pre_sympt)), pre_sympt, alpha=alpha, color='orange', label="pre_s")
-    handles.append(p)
-    p, = plt.plot(range(len(lat_sympt)),   lat_sympt,   alpha=alpha, color='b', label="latent_s")
-    handles.append(p)
-    p,  = plt.plot(range(len(recov)), recov, color='g', alpha=alpha, label="recovered")
-    handles.append(p)
-    p,  = plt.plot(range(len(infected_total)), infected_total, color='b', alpha=alpha, label="Total Infected")
-    handles.append(p)
+    alpha = 0.8
+
+    for n,l,c in zip(names, labels, colors):
+        p, = plt.plot(range(len(n)), n, alpha=alpha, color=c, label=l)
+        handles.append(p)
     return handles
+
 
 nb_runs = 5
 
