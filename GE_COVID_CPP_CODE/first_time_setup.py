99b5e6c3ed47655e0030a947742c14a9852d7a56
erlebach <gordon.erlebach@gmail.com>
2021-02-01 00:01:31 -0500

    More files.

--- GE_COVID_CPP_CODE/first_time_setup.py
+++ GE_COVID_CPP_CODE/first_time_setup.py
@@ -4,6 +4,12 @@ import os, sys
 cmd = "mkdir -p data_ge/results"
 os.system(cmd)
 
+if os.path.exists("data_ge/bak_gz/network.txt.gz"):
+    os.system("cd data_ge; cp bak_gz/network.txt.gz ..; gunzip network.txt.gz")
+
+if os.path.exists("data_ge/bak_gz/bak_gz/nodes.txt.gz"):
+    os.system("cd data_ge; cp bak_gz/nodes.txt.gz ..; gunzip nodes.txt.gz")
+
 os.system("cd data_ge; gunzip network.txt.gz nodes.txt.gz")
 
 
